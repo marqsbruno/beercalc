2e91dac6e27a8cc6cba3474ad9b2b922520ef5e7
marqsbruno <marqsbruno@gmail.com>
2024-07-15 23:06:47 -0300

    iniciando

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CalculatorComponent from './components/CalculatorComponent.vue'
+</script>
+
+<template>
+  <main>
+    <CalculatorComponent />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/CalculatorComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+const price = ref(0)
+const volume = ref(0)
+
+const handlePrice = (e) => {
+  price.value = e.target.value
+  console.log(price.value)
+}
+
+const handleVolume = (e) => {
+  volume.value = e.target.value
+  console.log(volume.value)
+}
+</script>
+
+<template>
+  <header>
+    <h1>Beer calculatorrr</h1>
+  </header>
+
+  <main>
+    <div>
+      <form>
+        <label for="price">Preço.</label>
+        <input type="number" name="price" id="price" step="any" v-on:change="handlePrice" />
+        <label for="ml">Volume em mililitros / ml.</label>
+        <input type="number" name="volume" id="volume" v-on:change="handleVolume" />
+      </form>
+    </div>
+    <div class="result">
+      <h2>{{ volume / price }}</h2>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
