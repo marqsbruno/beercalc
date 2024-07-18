9f173d5bda380d664be9009b72fbefc4f2cbbc85
marqsbruno <marqsbruno@gmail.com>
2024-07-18 11:55:11 -0300

    adiciona lógica

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
 # beercalc
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
 ## Project Setup
 
 ```sh
@@ -26,10 +16,4 @@ npm run dev
 
 ```sh
 npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
 ```
--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -3,34 +3,31 @@ import { ref } from 'vue'
 
 const price = ref(0)
 const volume = ref(0)
+const result = ref(0)
 
-const handlePrice = (e) => {
-  price.value = e.target.value
-  console.log(price.value)
-}
-
-const handleVolume = (e) => {
-  volume.value = e.target.value
-  console.log(volume.value)
+const handleCount = () => {
+  const CONVERT_ML = 1000
+  result.value = (price.value * CONVERT_ML) / volume.value
 }
 </script>
 
 <template>
   <header>
-    <h1>Beer calculatorrr</h1>
+    <h1>Beer calculator</h1>
   </header>
 
   <main>
     <div>
       <form>
+        <label for="ml">Volume em mililitros/ml.</label>
+        <input type="number" name="volume" id="volume" v-model="volume" />
         <label for="price">Preço.</label>
-        <input type="number" name="price" id="price" step="any" v-on:change="handlePrice" />
-        <label for="ml">Volume em mililitros / ml.</label>
-        <input type="number" name="volume" id="volume" v-on:change="handleVolume" />
+        <input type="number" name="price" id="price" min="0" step="0.01" v-model="price" />
       </form>
+      <button @click="handleCount">Go</button>
     </div>
     <div class="result">
-      <h2>{{ volume / price }}</h2>
+      <h2>R$ {{ result.toFixed(2).toString().replace('.', ',') }} por litro</h2>
     </div>
   </main>
 </template>
